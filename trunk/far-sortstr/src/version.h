d790c03ebebc171204839ebf879dd06a32413f18
andrew.grechkin@gmail.com <andrew.grechkin@gmail.com@b69ba8b2-28df-11df-ad47-1d73243ccc9f>
2010-11-23 15:06:16 +0000

    изменено поведение при откате изменений sortstr

--- trunk/far-sortstr/src/version.h
+++ trunk/far-sortstr/src/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "22";
+	static const char DATE[] = "23";
 	static const char MONTH[] = "11";
 	static const char YEAR[] = "2010";
 	static const char UBUNTU_VERSION_STYLE[] = "10.11";
@@ -15,18 +15,18 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR = 1;
-	static const long MINOR = 1;
-	static const long BUILD = 5;
-	static const long REVISION = 28;
+	static const long MINOR = 2;
+	static const long BUILD = 7;
+	static const long REVISION = 29;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 99;
-	#define RC_FILEVERSION 1,1,5,28
-	#define RC_FILEVERSION_STRING "1, 1, 5, 28\0"
-	static const char FULLVERSION_STRING[] = "1.1.5.28";
+	#define RC_FILEVERSION 1,2,7,29
+	#define RC_FILEVERSION_STRING "1, 2, 7, 29\0"
+	static const char FULLVERSION_STRING[] = "1.2.7.29";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 5;
+	static const long BUILD_HISTORY = 6;
 	
 
 }
