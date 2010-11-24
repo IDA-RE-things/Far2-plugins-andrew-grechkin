d6e08a2dd8d9b3685ff184e8c488187a45def756
andrew.grechkin@gmail.com <andrew.grechkin@gmail.com@b69ba8b2-28df-11df-ad47-1d73243ccc9f>
2010-11-24 16:09:30 +0000

    Добавил удаление дубликатов строк Добавил сортировку только выделения вертикального блока

--- trunk/far-sortstr/src/version.h
+++ trunk/far-sortstr/src/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "23";
+	static const char DATE[] = "24";
 	static const char MONTH[] = "11";
 	static const char YEAR[] = "2010";
 	static const char UBUNTU_VERSION_STYLE[] = "10.11";
@@ -15,18 +15,18 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR = 1;
-	static const long MINOR = 2;
-	static const long BUILD = 7;
-	static const long REVISION = 29;
+	static const long MINOR = 3;
+	static const long BUILD = 9;
+	static const long REVISION = 38;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 99;
-	#define RC_FILEVERSION 1,2,7,29
-	#define RC_FILEVERSION_STRING "1, 2, 7, 29\0"
-	static const char FULLVERSION_STRING[] = "1.2.7.29";
+	static const long BUILDS_COUNT = 337;
+	#define RC_FILEVERSION 1,3,9,38
+	#define RC_FILEVERSION_STRING "1, 3, 9, 38\0"
+	static const char FULLVERSION_STRING[] = "1.3.9.38";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 6;
+	static const long BUILD_HISTORY = 7;
 	
 
 }
