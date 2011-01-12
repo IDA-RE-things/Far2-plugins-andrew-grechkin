2f95a0f7627e7e257eb63d912d0fc699184cf7b8
andrew.grechkin <andrew.grechkin@gmail.com>
2011-01-12 04:51:09 +0000

    численная сортировка

--- far-sortstr/src/sortstr.cpp
+++ far-sortstr/src/sortstr.cpp
@@ -18,9 +18,12 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
-#include <win_std.h>
+#include <libwin_def/std.h>
+#include <libwin_def/reg.h>
 
-#include <far/helper.hpp>
+#include <math.h>
+
+#include <far/helper.h>
 
 #include <vector>
 using std::vector;
@@ -32,14 +35,21 @@ using std::pair;
 PCWSTR plug_name = L"sortstr";
 
 struct SelInfo {
-	intmax_t start;
-	intmax_t count;
-	SelInfo(intmax_t s = -1, intmax_t c = -1): start(s), count(c) {
+	ssize_t start;
+	ssize_t count;
+	SelInfo(ssize_t s = -1, ssize_t c = -1): start(s), count(c) {
+	};
+};
+
+struct SortInfo {
+	size_t line;
+	double num;
+	SortInfo(size_t l): line(l), num(0) {
 	};
 };
 
 typedef pair<AutoUTF, SelInfo> datapair;
-typedef pair<AutoUTF, size_t> sortpair;
+typedef pair<AutoUTF, SortInfo> sortpair;
 typedef vector<datapair>	data_vector;
 typedef vector<sortpair>	sort_vector;
 
@@ -52,6 +62,7 @@ enum {
 enum {
 	cbInvert = 5,
 	cbSensitive,
+	cbNumeric,
 	cbSelected,
 	cbAsEmpty,
 	txOperation,
@@ -68,7 +79,31 @@ EditorInfo ei;
 size_t	lineFirst;
 
 Register reg;
-int inv, cs, sel, emp, op;
+int inv, cs, ns, sel, emp, op;
+
+double	FindNum(PCWSTR str) {
+	double ret = HUGE_VAL;
+	PCWSTR num = (PCWSTR)(str + ::wcscspn(str, L"0123456789"));
+
+	if (*num) {
+		if (*num == L'0') {
+			ret = wcstol(num, nullptr, 0);
+		} else {
+			WCHAR buf[64];
+			WinMem::Copy(buf, num, std::min(sizeof(buf), Len(num) * sizeof(WCHAR)));
+			for (PWSTR k = buf; *k; ++k) {
+				if (*k == L',') {
+					*k = L'.';
+				}
+			}
+			ret = wcstod(num, nullptr);
+		}
+		if (num > str && num[-1] == L'-') {
+			ret = -ret;
+		}
+	}
+	return ret;
+}
 
 bool	PairEqCI(const sortpair &lhs, const sortpair &rhs) {
 	return	Cmpi(lhs.first.c_str(), rhs.first.c_str()) == 0;
@@ -82,8 +117,12 @@ bool	PairEqCScode(const sortpair &lhs, const sortpair &rhs) {
 	return	CmpCode(lhs.first.c_str(), rhs.first.c_str()) == 0;
 }
 
-bool	PairLessNum(const sortpair &lhs, const sortpair &rhs) {
-	return	lhs.second < rhs.second;
+bool	PairEqNum(const sortpair &lhs, const sortpair &rhs) {
+	return	lhs.second.num == rhs.second.num;
+}
+
+bool	PairLessLine(const sortpair &lhs, const sortpair &rhs) {
+	return	lhs.second.line < rhs.second.line;
 }
 
 bool	PairLessCI(const sortpair &lhs, const sortpair &rhs) {
@@ -91,7 +130,7 @@ bool	PairLessCI(const sortpair &lhs, const sortpair &rhs) {
 	if (ret < 0)
 		return	true;
 	else if (ret == 0)
-		return lhs.second < rhs.second;
+		return lhs.second.line < rhs.second.line;
 	return false;
 }
 
@@ -100,7 +139,7 @@ bool	PairLessCS(const sortpair &lhs, const sortpair &rhs) {
 	if (ret < 0)
 		return	true;
 	else if (ret == 0)
-		return lhs.second < rhs.second;
+		return lhs.second.line < rhs.second.line;
 	return false;
 }
 
@@ -109,8 +148,16 @@ bool	PairLessCScode(const sortpair &lhs, const sortpair &rhs) {
 	if (ret < 0)
 		return	true;
 	else if (ret == 0)
-		return lhs.second < rhs.second;
+		return lhs.second.line < rhs.second.line;
 	return false;
+}
+
+bool	PairLessNum(const sortpair &lhs, const sortpair &rhs) {
+	if (lhs.second.num < rhs.second.num)
+		return true;
+	else if (lhs.second.num == rhs.second.num)
+		return lhs.second.line < rhs.second.line;
+	return	false;
 }
 
 template <typename Type>
@@ -120,18 +167,18 @@ void	InsertFromVector(const data_vector &data, Type it, Type end) {
 		if (data[j].second.count == -2 && !emp) {
 			continue;
 		}
-		if (j == it->second) {
+		if (j == it->second.line) {
 			++it;
 			continue;
 		}
 		switch (op) {
 			case DEL_NO:
 				if (ei.BlockType == BTYPE_STREAM) {
-					Editor::SetString(i, data[it->second].first);
+					Editor::SetString(i, data[it->second.line].first);
 				} else {
 					if (sel) {
-						if (data[j].first.size() <= (uintmax_t)data[j].second.start) {
-							AutoUTF tmp(data[j].second.start, L' ');
+						if (data[j].first.size() <= (size_t)data[j].second.start) {
+							AutoUTF tmp(data[j].second.start, SPACE_C);
 							tmp.replace(0, data[j].first.size(), data[j].first);
 							tmp += it->first;
 							Editor::SetString(i, tmp);
@@ -141,18 +188,18 @@ void	InsertFromVector(const data_vector &data, Type it, Type end) {
 							Editor::SetString(i, tmp);
 						}
 					} else {
-						Editor::SetString(i, data[it->second].first);
+						Editor::SetString(i, data[it->second.line].first);
 					}
 				}
 				++it;
 				break;
 			case DEL_BLOCK:
-				Editor::SetString(i, data[it->second].first);
+				Editor::SetString(i, data[it->second.line].first);
 				++it;
 				break;
 			case DEL_SPARSE: {
 				if (!data[j].first.empty()) {
-					Editor::SetString(i, L"");
+					Editor::SetString(i, EMPTY);
 				}
 				break;
 			}
@@ -162,25 +209,24 @@ void	InsertFromVector(const data_vector &data, Type it, Type end) {
 	switch (op) {
 		case DEL_BLOCK:
 			for (; j < data.size(); ++i, ++j)
-				Editor::SetString(i, L"");
+				Editor::SetString(i, EMPTY);
 			break;
 		case DEL_SPARSE: {
 			for (; j < data.size(); ++i, ++j)
 				if (!data[j].first.empty())
-					Editor::SetString(i, L"");
+					Editor::SetString(i, EMPTY);
 		}
 	}
 }
 
 bool	ProcessEditor() {
-	data_vector	data;
-	sort_vector	sortdata;
-
 	if (ei.BlockType == BTYPE_STREAM || ei.BlockType == BTYPE_COLUMN)
 		lineFirst = ei.BlockStartLine;
 	else
 		lineFirst = 0;
 
+	data_vector	data;
+	sort_vector	sortdata;
 //	data.reserve(ei.TotalLines - lineFirst);
 //	sortdata.reserve(data.capacity());
 
@@ -197,7 +243,7 @@ bool	ProcessEditor() {
 
 		AutoUTF	tmp(egs.StringText, egs.StringLength);
 
-		intmax_t	SelLen = -2;
+		ssize_t	SelLen = -2;
 		switch (ei.BlockType) {
 			case BTYPE_COLUMN: {
 				if (egs.SelStart < egs.StringLength) {
@@ -205,6 +251,9 @@ bool	ProcessEditor() {
 				}
 				if (SelLen != -2) {
 					sortdata.push_back(sortpair(AutoUTF(egs.StringText + egs.SelStart, SelLen), i - lineFirst));
+					if (ns) {
+						sortdata.back().second.num = FindNum(sortdata.back().first.c_str());
+					}
 				} else if (emp) {
 					sortdata.push_back(sortpair(AutoUTF(), i - lineFirst));
 				}
@@ -215,27 +264,30 @@ bool	ProcessEditor() {
 			default:
 				data.push_back(data_vector::value_type(tmp, SelInfo(0, egs.StringLength)));
 				sortdata.push_back(sortpair(tmp, i - lineFirst));
+				if (ns) {
+					sortdata.back().second.num = FindNum(sortdata.back().first.c_str());
+				}
 		}
-//		file << "num: " << i << " sta: " << data[i].second.start << " cnt: " << data[i].second.count << endl;
+//		file << "line: " << i << " sta: " << data[i].second.start << " cnt: " << data[i].second.count << endl;
 	}
 
 //	ofstream file1("sortdata1.log");
 //	for (sort_vector::iterator it = sortdata.begin(); it != sortdata.end(); ++it) {
-//		file1 << "num: " << it->second << " str: '" << oem(it->first).c_str() << "'" << endl;
+//		file1 << "line: " << it->second.line << " num: " << it->second.num << " str: '" << oem(it->first).c_str() << "'" << endl;
 //	}
 
 	std::pointer_to_binary_function<const sortpair&, const sortpair&, bool>
-	pfLe(ptr_fun(PairLessCScode)),
-	pfEq(ptr_fun(PairEqCScode));
+	pfLe((ns) ? ptr_fun(PairLessNum) : ptr_fun(PairLessCScode)),
+	pfEq((ns) ? ptr_fun(PairEqNum) : ptr_fun(PairEqCScode));
 
 	switch (cs) {
 		case 0:
-			pfLe = ptr_fun(PairLessCI);
-			pfEq = ptr_fun(PairEqCI);
+			pfLe = (ns) ? ptr_fun(PairLessNum) : ptr_fun(PairLessCI);
+			pfEq = (ns) ? ptr_fun(PairEqNum) : ptr_fun(PairEqCI);
 			break;
 		case 1:
-			pfLe = ptr_fun(PairLessCS);
-			pfEq = ptr_fun(PairEqCS);
+			pfLe = (ns) ? ptr_fun(PairLessNum) : ptr_fun(PairLessCS);
+			pfEq = (ns) ? ptr_fun(PairEqNum) : ptr_fun(PairEqCS);
 			break;
 	}
 
@@ -244,12 +296,12 @@ bool	ProcessEditor() {
 	if (op) {
 		sort_vector::iterator it = std::unique(sortdata.begin(), sortdata.end(), pfEq);
 		sortdata.erase(it, sortdata.end());
-		std::sort(sortdata.begin(), sortdata.end(), ptr_fun(PairLessNum));
+		std::sort(sortdata.begin(), sortdata.end(), ptr_fun(PairLessLine));
 	}
 
 //	ofstream file4("sortdata4.log");
 //	for (sort_vector::iterator it = sortdata.begin(); it != sortdata.end(); ++it) {
-//		file4 << "num: " << it->second << " str: '" << oem(it->first).c_str() << "'" << endl;
+//		file4 << "line: " << it->second.line << " num: " << it->second.num << " str: '" << oem(it->first).c_str() << "'" << endl;
 //	}
 
 	Editor::StartUndo();
@@ -265,24 +317,6 @@ bool	ProcessEditor() {
 	return	true;
 }
 
-//▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓                                                            ▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ╔═══════════════════ Sort strings ═══════════════════╗   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ║ [ ] s Selection only                               ║   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ║ [ ] i Invert sort                                  ║   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ║ [ ] a Case sensitive                               ║   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ╟────────────────────────────────────────────────────╢   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ║ p Operation:                                       ║   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ║ Sort                                              ↓║   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ╟────────────────────────────────────────────────────╢   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ║               { o ok } [ c cancel ]                ║   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓   ╚════════════════════════════════════════════════════╝   ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓                                                            ░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-//▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-
 ///========================================================================================== Export
 void WINAPI		EXP_NAME(GetPluginInfo)(PluginInfo *pi) {
 	pi->StructSize = sizeof(PluginInfo);
@@ -293,7 +327,7 @@ void WINAPI		EXP_NAME(GetPluginInfo)(PluginInfo *pi) {
 	pi->PluginMenuStringsNumber = 1;
 }
 
-HANDLE WINAPI	EXP_NAME(OpenPlugin)(int OpenFrom, INT_PTR Item) {
+HANDLE WINAPI	EXP_NAME(OpenPlugin)(int /*OpenFrom*/, INT_PTR /*Item*/) {
 	psi.EditorControl(ECTL_GETINFO, &ei);
 	static FarListItem litems[] = {
 		{0, GetMsg(lbSort), {0}},
@@ -303,26 +337,28 @@ HANDLE WINAPI	EXP_NAME(OpenPlugin)(int OpenFrom, INT_PTR Item) {
 	static FarList flist = {sizeofa(litems), litems};
 
 	enum {
-		HEIGHT = 14,
+		HEIGHT = 15,
 		WIDTH = 60,
 
 		indInv = 1,
 		indCS,
-		indSelected = 4,
+		indNS,
+		indSelected = 5,
 		indAsEmpty,
-		indList = 8,
+		indList = 9,
 	};
 	InitDialogItemF	Items[] = {
 		{DI_DOUBLEBOX, 3,  1,  WIDTH - 4, HEIGHT - 2, 0, (PCWSTR)DlgTitle},
 		{DI_CHECKBOX,  5, 2, 28,  0,                  0, (PCWSTR)cbInvert},
 		{DI_CHECKBOX,  5, 3, 28,  0,                  DIF_3STATE, (PCWSTR)cbSensitive},
-		{DI_TEXT,      0, 4, 0,  0,                   DIF_SEPARATOR,   L""},
-		{DI_CHECKBOX,  5, 5, 0,  0,                   0, (PCWSTR)cbSelected},
-		{DI_CHECKBOX,  5, 6, 0,  0,                   0, (PCWSTR)cbAsEmpty},
-		{DI_TEXT,      0, HEIGHT - 7, 0,  0,          DIF_SEPARATOR,   L""},
+		{DI_CHECKBOX,  5, 4, 28,  0,                  0, (PCWSTR)cbNumeric},
+		{DI_TEXT,      0, 5, 0,  0,                   DIF_SEPARATOR,   EMPTY},
+		{DI_CHECKBOX,  5, 6, 0,  0,                   0, (PCWSTR)cbSelected},
+		{DI_CHECKBOX,  5, 7, 0,  0,                   0, (PCWSTR)cbAsEmpty},
+		{DI_TEXT,      0, HEIGHT - 7, 0,  0,          DIF_SEPARATOR,   EMPTY},
 		{DI_TEXT,      5, HEIGHT - 6, 0,  0,          0, (PCWSTR)txOperation},
 		{DI_COMBOBOX,  5, HEIGHT - 5, 54,  0,         DIF_DROPDOWNLIST | DIF_LISTNOAMPERSAND, (PCWSTR)lbSort},
-		{DI_TEXT,      0,  HEIGHT - 4, 0,  0,         DIF_SEPARATOR,   L""},
+		{DI_TEXT,      0,  HEIGHT - 4, 0,  0,         DIF_SEPARATOR,   EMPTY},
 		{DI_BUTTON,    0,  HEIGHT - 3, 0,  0,         DIF_CENTERGROUP, (PCWSTR)txtBtnOk},
 		{DI_BUTTON,    0,  HEIGHT - 3, 0,  0,         DIF_CENTERGROUP, (PCWSTR)txtBtnCancel},
 	};
@@ -332,6 +368,7 @@ HANDLE WINAPI	EXP_NAME(OpenPlugin)(int OpenFrom, INT_PTR Item) {
 	FarItems[size - 2].DefaultButton = 1;
 	FarItems[indInv].Selected = inv;
 	FarItems[indCS].Selected = cs;
+	FarItems[indNS].Selected = ns;
 	if (ei.BlockType != BTYPE_COLUMN) {
 		FarItems[indSelected].Flags |= DIF_DISABLE;
 		FarItems[indAsEmpty].Flags |= DIF_DISABLE;
@@ -346,12 +383,14 @@ HANDLE WINAPI	EXP_NAME(OpenPlugin)(int OpenFrom, INT_PTR Item) {
 		if (ret > 0 && Items[ret].Data == (PCWSTR)txtBtnOk) {
 			inv = hDlg.Check(indInv);
 			cs = hDlg.Check(indCS);
+			ns = hDlg.Check(indNS);
 			sel = hDlg.Check(indSelected);
 			emp = hDlg.Check(indAsEmpty);
 			op = psi.SendDlgMessage(hDlg, DM_LISTGETCURPOS, indList, nullptr);
 
 			reg.Set(L"invert", inv);
 			reg.Set(L"case", cs);
+			reg.Set(L"numeric", ns);
 			reg.Set(L"selection", sel);
 			reg.Set(L"asempty", emp);
 
@@ -364,10 +403,11 @@ HANDLE WINAPI	EXP_NAME(OpenPlugin)(int OpenFrom, INT_PTR Item) {
 void WINAPI		EXP_NAME(SetStartupInfo)(const PluginStartupInfo *psi) {
 	InitFSF(psi);
 
-	reg.Open(KEY_READ | KEY_WRITE, AutoUTF(psi->RootKey) + L"\\" + plug_name);
+	reg.Open(KEY_READ | KEY_WRITE, (AutoUTF(psi->RootKey) + PATH_SEPARATOR + plug_name).c_str());
 
 	reg.Get(L"invert", inv, 0);
 	reg.Get(L"case", cs, 0);
+	reg.Get(L"numeric", ns, 0);
 	reg.Get(L"selection", sel, 0);
 	reg.Get(L"asempty", emp, 0);
 	op = 0;
--- far-sortstr/src/version.h
+++ far-sortstr/src/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "26";
-	static const char MONTH[] = "11";
-	static const char YEAR[] = "2010";
-	static const char UBUNTU_VERSION_STYLE[] = "10.11";
+	static const char DATE[] = "12";
+	static const char MONTH[] = "01";
+	static const char YEAR[] = "2011";
+	static const char UBUNTU_VERSION_STYLE[] = "11.01";
 	
 	//Software Status
 	static const char STATUS[] = "Alpha";
@@ -15,15 +15,15 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR = 1;
-	static const long MINOR = 4;
-	static const long BUILD = 13;
+	static const long MINOR = 5;
+	static const long BUILD = 14;
 	static const long REVISION = 53;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 1;
-	#define RC_FILEVERSION 1,4,13,53
-	#define RC_FILEVERSION_STRING "1, 4, 13, 53\0"
-	static const char FULLVERSION_STRING[] = "1.4.13.53";
+	#define RC_FILEVERSION 1,5,14,53
+	#define RC_FILEVERSION_STRING "1, 5, 14, 53\0"
+	static const char FULLVERSION_STRING[] = "1.5.14.53";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 10;
